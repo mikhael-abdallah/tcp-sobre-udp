2f2ab7386c4bcf4e7a4a6fffb8bf4f9e33c14f57
Mikhael Abdallah de Oliveira Pinto <mikhael.pinto@germinare.org.br>
2022-12-29 10:24:26 -0300

    função para criar arquivo

---
+++ src/main/java/org/tcp/CriaArquivo.java
@@ -0,0 +1,23 @@
+package org.tcp;
+
+import java.io.*;
+
+public class CriaArquivo {
+    public static void main(String[] args) throws IOException {
+        File fout = new File("file.txt");
+        FileOutputStream fos = new FileOutputStream(fout);
+
+        long fileSize = 583000; //100MB
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+
+        int i = 0;
+        while(fout.length() < fileSize) {
+            bw.write(Integer.toString(i));
+            bw.newLine();
+            i++;
+        }
+
+        bw.close();
+    }
+}
